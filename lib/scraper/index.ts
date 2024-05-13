c977ede3c7defac4fb4f7922ee7401b3a201fba3
komal paul <149079566+PaulWebdeveloper7@users.noreply.github.com>
2024-05-13 19:24:04 +0530

    testing

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -68,6 +68,7 @@ const description = extractDescription($)
     highestPrice: Number(originalPrice) || Number(currentPrice),
     averagePrice: Number(currentPrice) || Number(originalPrice),
   }
+  console.log(data)
   return data;
    } 
    catch (error:any) {
