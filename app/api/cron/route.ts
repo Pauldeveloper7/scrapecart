4df9061f3b7ef22cbcda47aed51642ff872db04b
komal paul <149079566+PaulWebdeveloper7@users.noreply.github.com>
2024-05-18 08:02:24 +0530

    add

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 import {generateEmailBody , sendEmail} from '@/lib/nodemailer'
 
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 60; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
